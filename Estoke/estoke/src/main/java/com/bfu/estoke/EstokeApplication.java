4dd47bc7ebf74a8380aabd7b1c23dadf8116413b
AlissonSouto7 <alissonprogp1@gmail.com>
2024-01-23 11:16:44 -0300

    First Commit

---
+++ Estoke/estoke/src/main/java/com/bfu/estoke/EstokeApplication.java
@@ -0,0 +1,13 @@
+package com.bfu.estoke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstokeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstokeApplication.class, args);
+	}
+
+}
